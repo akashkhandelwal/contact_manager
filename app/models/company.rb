32df9fb324b3da1f4fb34820ec2c85677096b853
Akash Khadelwal <akash.khandelwal@jombay.com>
2013-06-25 17:16:03 +0530

    Add 'to_s' method

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,8 @@ class Company < ActiveRecord::Base
   attr_accessible :name
 
   validates :name, presence: true
+
+  def to_s
+  	"#{name}"
+  end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -4,4 +4,9 @@ class Person < ActiveRecord::Base
   attr_accessible :first_name, :last_name
 
   validates :first_name, :last_name, presence: true
+
+  def to_s
+	  "#{last_name}, #{first_name}"
+	end
+
 end
